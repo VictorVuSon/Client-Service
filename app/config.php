91105ddf14a259e566cb067fe21cc6a93f60f658
Victor son <ngocson.bkdn@gmail.com>
2016-11-10 21:15:51 +0700

    first commit

---
+++ app/config.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ *
+Author: vu joker
+Author URL: facebook.com/victor.vuson
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+ *
+ */
+$current_page_uri = $_SERVER['REQUEST_URI'];
+$part_url = explode("/", $current_page_uri);
+$page_name = end($part_url);
+$email_id = "ngocson@gmail.com";
+?>
