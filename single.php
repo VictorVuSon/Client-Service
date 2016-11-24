b43b323948e9016cea009d9054915f1ada031ee8
duyenpham <phamduyen9694@gmail.com>
2016-11-24 09:37:26 +0700

    homepage,load list restaurant

--- single.php
+++ single.php
@@ -16,161 +16,184 @@ require_once 'web/inc/header.php';
 <script type="text/javascript" src="web/js/megamenu.js"></script>
 <script>$(document).ready(function(){$(".megamenu").megamenu();});</script> -->
 <script type="text/javascript" src="web/js/jquery.jscrollpane.min.js"></script>
-		<script type="text/javascript" id="sourcecode">
-			$(function()
-			{
-				$('.scroll-pane').jScrollPane();
-			});
-		</script>
+<script type="text/javascript" id="sourcecode">
+    $(function ()
+    {
+        $('.scroll-pane').jScrollPane();
+    });
+</script>
 <!-- start details -->
 <script src="web/js/slides.min.jquery.js"></script>
-   <script>
-		$(function(){
-			$('#products').slides({
-				preload: true,
-				preloadImage: 'img/loading.gif',
-				effect: 'slide, fade',
-				crossfade: true,
-				slideSpeed: 350,
-				fadeSpeed: 500,
-				generateNextPrev: true,
-				generatePagination: false
-			});
-		});
-	</script>
+<script>
+    $(function () {
+        $('#products').slides({
+            preload: true,
+            preloadImage: 'img/loading.gif',
+            effect: 'slide, fade',
+            crossfade: true,
+            slideSpeed: 350,
+            fadeSpeed: 500,
+            generateNextPrev: true,
+            generatePagination: false
+        });
+    });
+</script>
 <link rel="stylesheet" href="web/css/etalage.css">
 <script src="web/js/jquery.etalage.min.js"></script>
 <script>
-			jQuery(document).ready(function($){
+    jQuery(document).ready(function ($) {
 
-				$('#etalage').etalage({
-					thumb_image_width: 360,
-					thumb_image_height: 360,
-					source_image_width: 900,
-					source_image_height: 900,
-					show_hint: true,
-					click_callback: function(image_anchor, instance_id){
-						alert('Callback example:\nYou clicked on an image with the anchor: "'+image_anchor+'"\n(in Etalage instance: "'+instance_id+'")');
-					}
-				});
+        $('#etalage').etalage({
+            thumb_image_width: 360,
+            thumb_image_height: 360,
+            source_image_width: 900,
+            source_image_height: 900,
+            show_hint: true,
+            click_callback: function (image_anchor, instance_id) {
+                alert('Callback example:\nYou clicked on an image with the anchor: "' + image_anchor + '"\n(in Etalage instance: "' + instance_id + '")');
+            }
+        });
 
-			});
-		</script>	
+    });
+</script>
+<script>
+function loadHomePage() {
+        var position = 0;
+        var divId = 0;
+        var isChange = false;
+        $.ajax({
+            url: 'http://localhost/userYii2/api/web/index.php/restaurant/index',
+            type: 'GET',
+            cache: false,
+            headers: {Authorization: TOKEN},
+            success: function (data) {
+                var arrRestaurant = data.data;
+                console.log(arrRestaurant);
+                arrRestaurant.forEach(function (restaurant) {
+                    isChange = false;
+                    position++;
+                    if((((position - 1) % 3) === 0) && (position != 1)){
+                        isChange = true;
+                        divId ++ ;
+                    }
+                    var name = restaurant.name;
+                    var id = restaurant.id;
+                    var address = restaurant.address.name+'-'+ restaurant.address.number +'-'+restaurant.address.street +'-'+ restaurant.address.district.name +'-'+ restaurant.address.district.city.name;
+                    var rate = restaurant.point ;
+                    var image = "http://localhost/userYii2/backend/web/uploads/"+restaurant.image;
+                  //  addElement(id, name, address, rate, image, isChange, divId);
+                });
+            },
+            error: function () {
+                alert('Something went wrong!');
+            }
+        });
+        return false;
+    }
+</script>
 </head>
 <body>
-<div class="mens">    
-  <div class="main">
-     <div class="wrap">
-     	<ul class="breadcrumb breadcrumb__t"><a class="home" href="#">Home</a> / <a href="#">Restaurant</a> / Nha hang thien duong</ul>
-		<div class="cont span_2_of_3">
-		  	<div class="grid images_3_of_2">
-						<ul id="etalage">
-							<li>
-								<a href="optionallink.php">
-									<img class="etalage_thumb_image" src="web/images/s-img.jpg" class="img-responsive" />
-									<img class="etalage_source_image" src="web/images/s1.jpg" class="img-responsive" title="" />
-								</a>
-							</li>
-							<!-- <li>
-								<img class="etalage_thumb_image" src="web/images/s-img1.jpg" class="img-responsive" />
-								<img class="etalage_source_image" src="web/images/s2.jpg" class="img-responsive" title="" />
-							</li>
-							<li>
-								<img class="etalage_thumb_image" src="web/images/s-img2.jpg" class="img-responsive"  />
-								<img class="etalage_source_image" src="web/images/s3.jpg" class="img-responsive"  />
-							</li>
-						    <li>
-								<img class="etalage_thumb_image" src="web/images/s4.jpg" class="img-responsive"  />
-								<img class="etalage_source_image" src="web/images/s-img3.jpg" class="img-responsive"  />
-							</li> -->
-						</ul>
-						 <div class="clearfix"></div>
-	            </div>
-		         <div class="desc1 span_3_of_2">
-		         	<h3 class="m_3">Nha Hang com nieu</h3>
-		         	<h3 >Dia Chi: 123 Hung vuong, Da Nang</h3>
-		         	<h3 >Gio mo cua: 7:00 -22:00</h3>
-		         	<h3 >Kieu nha hang:</h3>
-		             <p class="m_5">Gia : 30.000 : 50.000 vnd</p> 
-		         	 <div class="btn_form">
-						<form>
-							<input type="submit" value="Rate" title="">
-						</form>
-					 </div>
-					<!-- <span class="m_link">Mo ta: </span>
-				     <p class="m_text2">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit </p> -->
-			     </div>
-			   <div class="clear"></div>	
-	    <div class="clients">
-	    <h3 class="m_3">Mon an trong nha hang</h3>
-		 <ul id="flexiselDemo3">
-			<li><img src="web/images/s5.jpg" /><a href="#">Ten mon</a><p>Gia:</p></li>
-			<li><img src="web/images/s5.jpg" /><a href="#">Ten mon</a><p>Gia:</p></li>
-			<li><img src="web/images/s5.jpg" /><a href="#">Ten mon</a><p>Gia:</p></li>
-			<li><img src="web/images/s5.jpg" /><a href="#">Ten mon</a><p>Gia:</p></li>
-		 </ul>
-	<script type="text/javascript">
-		$(window).load(function() {
-			$("#flexiselDemo1").flexisel();
-			$("#flexiselDemo2").flexisel({
-				enableResponsiveBreakpoints: true,
-		    	responsiveBreakpoints: { 
-		    		portrait: { 
-		    			changePoint:480,
-		    			visibleItems: 1
-		    		}, 
-		    		landscape: { 
-		    			changePoint:640,
-		    			visibleItems: 2
-		    		},
-		    		tablet: { 
-		    			changePoint:768,
-		    			visibleItems: 3
-		    		}
-		    	}
-		    });
-		
-			$("#flexiselDemo3").flexisel({
-				visibleItems: 5,
-				animationSpeed: 1000,
-				autoPlay: false,
-				autoPlaySpeed: 3000,    		
-				pauseOnHover: true,
-				enableResponsiveBreakpoints: true,
-		    	responsiveBreakpoints: { 
-		    		portrait: { 
-		    			changePoint:480,
-		    			visibleItems: 1
-		    		}, 
-		    		landscape: { 
-		    			changePoint:640,
-		    			visibleItems: 2
-		    		},
-		    		tablet: { 
-		    			changePoint:768,
-		    			visibleItems: 3
-		    		}
-		    	}
-		    });
-		    
-		});
-	</script>
-	<script type="text/javascript" src="web/js/jquery.flexisel.js"></script>
-     </div>
-     <div class="toogle">
-     	<h3 class="m_3">Thong tin nha hang</h3>
-     	<p class="m_text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum.</p>
-     </div>
-           </div>
-			
-	 <?php
-	require_once 'web/inc/menuSearch.php';
-	?>
+    <div class="mens">    
+        <div class="main">
+            <div class="wrap">
+                <ul class="breadcrumb breadcrumb__t"><a class="home" href="#">Home</a> / <a href="#">Restaurant</a> / Nha hang thien duong</ul>
+                <div class="cont span_2_of_3">
+                    <div class="grid images_3_of_2">
+                        <ul id="etalage">
+                            <li>
+                                <a href="optionallink.php">
+                                    <img class="etalage_thumb_image" src="web/images/s-img.jpg" class="img-responsive" />
+                                    <img class="etalage_source_image" src="web/images/s1.jpg" class="img-responsive" title="" />
+                                </a>
+                            </li>
+                        </ul>
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="desc1 span_3_of_2">
+                        <h3 class="m_3">Nha Hang com nieu</h3>
+                        <h3 >Dia Chi: 123 Hung vuong, Da Nang</h3>
+                        <h3 >Gio mo cua: 7:00 -22:00</h3>
+                        <h3 >Kieu nha hang:</h3>
+                        <p class="m_5">Gia : 30.000 : 50.000 vnd</p> 
+                        <div class="btn_form">
+                            <form>
+                                <input type="submit" value="Rate" title="">
+                            </form>
+                        </div>
+                       <!-- <span class="m_link">Mo ta: </span>
+                    <p class="m_text2">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit </p> -->
+                    </div>
+                    <div class="clear"></div>	
+                    <div class="clients">
+                        <h3 class="m_3">Mon an trong nha hang</h3>
+                        <ul id="flexiselDemo3">
+                            <li><img src="web/images/s5.jpg" /><a href="#">Ten mon</a><p>Gia:</p></li>
+                            <li><img src="web/images/s5.jpg" /><a href="#">Ten mon</a><p>Gia:</p></li>
+                            <li><img src="web/images/s5.jpg" /><a href="#">Ten mon</a><p>Gia:</p></li>
+                            <li><img src="web/images/s5.jpg" /><a href="#">Ten mon</a><p>Gia:</p></li>
+                        </ul>
+                        <script type="text/javascript">
+                            $(window).load(function () {
+                                $("#flexiselDemo1").flexisel();
+                                $("#flexiselDemo2").flexisel({
+                                    enableResponsiveBreakpoints: true,
+                                    responsiveBreakpoints: {
+                                        portrait: {
+                                            changePoint: 480,
+                                            visibleItems: 1
+                                        },
+                                        landscape: {
+                                            changePoint: 640,
+                                            visibleItems: 2
+                                        },
+                                        tablet: {
+                                            changePoint: 768,
+                                            visibleItems: 3
+                                        }
+                                    }
+                                });
 
-			</div>
-			 <div class="clear"></div>
-		   </div>
-		</div>
- <?php
-require_once 'web/inc/footer.php';
-?>
+                                $("#flexiselDemo3").flexisel({
+                                    visibleItems: 5,
+                                    animationSpeed: 1000,
+                                    autoPlay: false,
+                                    autoPlaySpeed: 3000,
+                                    pauseOnHover: true,
+                                    enableResponsiveBreakpoints: true,
+                                    responsiveBreakpoints: {
+                                        portrait: {
+                                            changePoint: 480,
+                                            visibleItems: 1
+                                        },
+                                        landscape: {
+                                            changePoint: 640,
+                                            visibleItems: 2
+                                        },
+                                        tablet: {
+                                            changePoint: 768,
+                                            visibleItems: 3
+                                        }
+                                    }
+                                });
+
+                            });
+                        </script>
+                        <script type="text/javascript" src="web/js/jquery.flexisel.js"></script>
+                    </div>
+                    <div class="toogle">
+                        <h3 class="m_3">Thong tin nha hang</h3>
+                        <p class="m_text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum.</p>
+                    </div>
+                </div>
+
+                <?php
+                require_once 'web/inc/menuSearch.php';
+                ?>
+
+            </div>
+            <div class="clear"></div>
+        </div>
+    </div>
+    <?php
+    require_once 'web/inc/footer.php';
+    ?>
--- web/inc/header.php
+++ web/inc/header.php
@@ -6,78 +6,83 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 -->
 <!DOCTYPE HTML>
 <html>
-<head>
-<title>My Foody</title>
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link href="web/css/style.css" rel="stylesheet" type="text/css" media="all" />
-<link href="web/css/form.css" rel="stylesheet" type="text/css" media="all" />
-<link href='http://fonts.googleapis.com/css?family=Exo+2' rel='stylesheet' type='text/css'>
-<script type="text/javascript" src="web/js/jquery1.min.js"></script>
-<!-- start menu -->
-<link href="web/css/megamenu.css" rel="stylesheet" type="text/css" media="all" />
-<script type="text/javascript" src="web/js/megamenu.js"></script>
-<script>$(document).ready(function(){$(".megamenu").megamenu();});</script>
-<!--start slider -->
-    <link rel="stylesheet" href="web/css/fwslider.css" media="all">
-    <script src="web/js/jquery-ui.min.js"></script>
-    <script src="web/js/css3-mediaqueries.js"></script>
-    <script src="web/js/fwslider.js"></script>
-<!--end slider -->
-<script src="web/js/jquery.easydropdown.js"></script>
-</head>
-<body>
-     <div class="header-top">
-	   <div class="wrap"> 
-			 <div class="cssmenu">
-				<ul>
-					<li><a href="login.php">Log In</a></li> |
-					<li><a href="register.php">Sign Up</a></li>
-				</ul>
-			</div>
-			<div class="clear"></div>
- 		</div>
-	</div>
-	<div class="header-bottom">
-	    <div class="wrap">
-			<div class="header-bottom-left">
-				<div class="logo">
-					<a href="index.php"><img src="web/images/foody.png" style="width:150px" alt=""/></a>
-				</div>
-				<div class="menu">
-	            <ul class="megamenu skyblue">
-			<li class="active grid"><a href="index.php">Trang chu</a></li>
-			<li><a class="color4" href="#">Nha Hang moi</a>
-				</li>				
-				<li><a class="color5" href="#">Kieu Nha Hang</a>
-				<div class="megapanel">
-					<div class="col1">
-							<div class="h_nav">
-								<h4>Top tuan</h4>
-								<ul>
-									<li><a href="mens.php">Nha hang Y</a></li>
-									<li><a href="mens.php">Nha Hang Nhat</a></li>
-									<li><a href="mens.php">Nha Hang Trung Quoc</a></li>
-									<li><a href="mens.php">Nha hang Han Quoc</a></li>
-									<li><a href="mens.php">Thuc an nhanh</a></li>
-								</ul>	
-							</div>							
-						</div>
-					</div>
-				</li>
-				<li><a class="color6" href="other.php">Top tuan</a></li>
-			</ul>
-			</div>
-		</div>
-	   <div class="header-bottom-right">
-         <div class="search">	  
-				<input type="text" name="s" class="textbox" value="Search" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Search';}">
-				<input type="submit" value="Subscribe" id="submit" name="submit">
-				<div id="response"> </div>
-		 </div>
-	  <div class="tag-list">
-	  </div>
-    </div>
-     <div class="clear"></div>
-     </div>
-	</div>
+    <head>
+        <title>My Foody</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <link href="web/css/style.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="web/css/form.css" rel="stylesheet" type="text/css" media="all" />
+        <link href='http://fonts.googleapis.com/css?family=Exo+2' rel='stylesheet' type='text/css'>
+        <script type="text/javascript" src="web/js/jquery1.min.js"></script>
+        <!-- start menu -->
+        <link href="web/css/megamenu.css" rel="stylesheet" type="text/css" media="all" />
+        <script type="text/javascript" src="web/js/megamenu.js"></script>
+        <script>$(document).ready(function () {
+        $(".megamenu").megamenu();
+    });</script>
+        <!--start slider -->
+        <link rel="stylesheet" href="web/css/fwslider.css" media="all">
+        <script src="web/js/jquery-ui.min.js"></script>
+        <script src="web/js/css3-mediaqueries.js"></script>
+        <script src="web/js/fwslider.js"></script>
+        <script src="web/js/js_const.js"></script>
+        <!--end slider -->
+        <script src="web/js/jquery.easydropdown.js"></script>
+    </head>
+    <body>
+        <div class="header-top">
+            <div class="wrap"> 
+                <div class="cssmenu">
+                    <ul>
+                        <li><a href="login.php">Log In</a></li> |
+                        <li><a href="register.php">Sign Up</a></li>
+                    </ul>
+                </div>
+                <div class="clear"></div>
+            </div>
+        </div>
+        <div class="header-bottom">
+            <div class="wrap">
+                <div class="header-bottom-left">
+                    <div class="logo">
+                        <a href="index.php"><img src="web/images/foody.png" style="width:150px" alt=""/></a>
+                    </div>
+                    <div class="menu">
+                        <ul class="megamenu skyblue">
+                            <li class="active grid"><a href="index.php">Trang chu</a></li>
+                            <li><a class="color4" href="#">Nha Hang moi</a>
+                            </li>				
+                            <li><a class="color5" href="#">Kieu Nha Hang</a>
+                                <div class="megapanel">
+                                    <div class="col1">
+                                        <div class="h_nav">
+                                            <h4>Top tuan</h4>
+                                            <ul>
+                                                <li><a href="mens.php">Nha hang Y</a></li>
+                                                <li><a href="mens.php">Nha Hang Nhat</a></li>
+                                                <li><a href="mens.php">Nha Hang Trung Quoc</a></li>
+                                                <li><a href="mens.php">Nha hang Han Quoc</a></li>
+                                                <li><a href="mens.php">Thuc an nhanh</a></li>
+                                            </ul>	
+                                        </div>							
+                                    </div>
+                                </div>
+                            </li>
+                            <li><a class="color6" href="other.php">Top tuan</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="header-bottom-right">
+                    <div class="search">	  
+                        <input type="text" name="s" class="textbox" value="Search" onfocus="this.value = '';" onblur="if (this.value == '') {
+                                            this.value = 'Search';
+                                        }">
+                        <input type="submit" value="Subscribe" id="submit" name="submit">
+                        <div id="response"> </div>
+                    </div>
+                    <div class="tag-list">
+                    </div>
+                </div>
+                <div class="clear"></div>
+            </div>
+        </div>
